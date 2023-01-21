bda4a1f066d010757e6d5b02016ea86ad0053d5f
Wixonic <public.wixonic@icloud.com>
2023-01-21 18:11:23 +0100

    Remove unused librairies

--- libraries/firebase/analytics.js
+++
@@ -1,2 +0,0 @@
-import * as Analytics from "https://cdn.jsdelivr.net/npm/firebase@^9.16.0/firebase-analytics.min.js";
-export default Analytics;
--- libraries/firebase/app.js
+++
@@ -1,2 +0,0 @@
-import * as App from "https://cdn.jsdelivr.net/npm/firebase@^9.16.0/firebase-app.min.js";
-export default App;
